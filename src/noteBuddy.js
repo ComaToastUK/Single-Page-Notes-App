447a20eb0337465cd57048c5d41b6abc8f2a321d
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-10-09 16:57:48 +0100

    Created NoteBuddy

---
+++ src/noteBuddy.js
@@ -0,0 +1,17 @@
+(function(exports) {
+  function NoteBuddy() {
+    this.list = [];
+  }
+
+  NoteBuddy.prototype.storeNote = function(note) {
+    return this.list.push(note);
+  };
+
+  NoteBuddy.prototype.createNote = function(string) {
+    var note = new Note(string);
+    this.storeNote(note);
+    return note
+  };
+
+  exports.NoteBuddy = NoteBuddy;
+})(this);

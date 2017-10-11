69de84905706610f447ee3873b2fb07dfac503c6
ComaToastUK <jesusmanyouatethedog@gmail.com>
2017-10-11 12:18:18 +0100

    added link functionality for notelist items

--- spec/noteBuddyViewSpec.js
+++ spec/noteBuddyViewSpec.js
@@ -2,12 +2,11 @@
   function noteBuddyViewChecker() {
     noteBuddy = new NoteBuddy();
     noteBuddy.createNote("This is a note longer than 20 characters to check that it gets summarized.");
-    noteBuddy.createNote("Testing, testing - we should only see 20 characters of this.");
+    noteBuddy.createNote("Testing, testing we should only see 20 characters of this.");
     var noteBuddyView = new NoteBuddyView(noteBuddy);
     if (
       noteBuddyView.toHTML() !==
-      "<ul><li><div>This is a note longe</div></li><li><div>Testing, testing - w</div></li></ul>"
-    ) {
+      "<ul><div id='noteLink'><a href='#This-is-a-note-longe'>This is a note longe</a></div><div id='noteLink'><a href='#Testing,-testing-we-'>Testing, testing we </a></div></ul>"    ) {
       throw new Error("NoteBuddyView has malfunctioned");
     } else {
       console.log("NoteBuddyView.toHTML verified");

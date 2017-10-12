ed03ae327eb5afb911d5279d40c978ca0b832c32
ComaToastUK <jesusmanyouatethedog@gmail.com>
2017-10-12 17:22:16 +0100

    test_file

---
+++ PingPongTest/src/PingPongTest.js
@@ -0,0 +1,28 @@
+function PingPongSpec(){
+  this.expectation = null;
+  this._it = null;
+  this._testObject = null;
+}
+
+PingPongSpec.prototype.expect = function (expectation){
+  this.expectation = expectation;
+};
+
+PingPongSpec.prototype.it = function (testDescription) {
+  this._it = testDescription.toString();
+};
+
+
+PingPongSpec.prototype.toEqual = function (a,b) {
+    return a === b;
+};
+
+PingPongSpec.prototype.expectToEqual = function (object){
+  this._testObject = object;
+  if ( this.toEqual(object, this.expectation)) {
+    console.log(this._it + " PASSED");
+  } else {
+    console.log(this._it + " FAIL");
+    console.log("Expected " + this.expectation + " and got " + this._testObject);
+  }
+};
